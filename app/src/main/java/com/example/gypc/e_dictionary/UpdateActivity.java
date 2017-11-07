ca603d519a32d0d0477544aefa16e7d7dff35a21
郭瑶佳 <1483197714@qq.com>
2017-11-07 12:12:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gypc/e_dictionary/UpdateActivity.java
@@ -0,0 +1,15 @@
+package com.example.gypc.e_dictionary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gypc on 2017/11/7.
+ */
+
+public class UpdateActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
