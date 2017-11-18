ee323c3de27c53d4b06fa5afb8a15bd444e9d730
郭瑶佳 <1483197714@qq.com>
2017-11-18 16:37:12 +0800

    添加了UpdateAvatorActivity代码文件和activity_avator布局文件，同时修改Manifest文件。 activity_update添加提示信息。 activity_main出现绝对位置，删掉。

---
+++ app/src/main/java/com/example/gypc/e_dictionary/UpdateAvatorActivity.java
@@ -0,0 +1,15 @@
+package com.example.gypc.e_dictionary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gypc on 2017/11/18.
+ */
+
+public class UpdateAvatorActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avator);
+    }
+}
