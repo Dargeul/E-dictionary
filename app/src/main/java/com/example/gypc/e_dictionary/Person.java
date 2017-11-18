7d790eb85e72278e79c80cf0890430a477e0f246
xujj25 <xujj25@foxmail.com>
2017-11-19 00:06:41 +0800

    更新人物类

--- app/src/main/java/com/example/gypc/e_dictionary/Person.java
+++ app/src/main/java/com/example/gypc/e_dictionary/Person.java
@@ -5,4 +5,12 @@ package com.example.gypc.e_dictionary;
  */
 
 public class Person {
+    public int personId;
+    public int avatarIndex;
+    public String name;
+    public String country;
+    public String nickName;
+    public int startYear;
+    public int endYear;
+    public String birthplace;
 }
