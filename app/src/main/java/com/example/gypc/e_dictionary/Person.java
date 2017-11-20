5bc10317cabeada0f07b0acabfddbca3c0883d5b
xujj25 <xujj25@foxmail.com>
2017-11-21 00:14:39 +0800

    修改接口

--- app/src/main/java/com/example/gypc/e_dictionary/Person.java
+++ app/src/main/java/com/example/gypc/e_dictionary/Person.java
@@ -1,5 +1,7 @@
 package com.example.gypc.e_dictionary;
 
+import android.util.Log;
+
 /**
  * Created by XUJIJUN on 2017/11/18.
  */
@@ -13,4 +15,19 @@ public class Person {
     public int startYear;
     public int endYear;
     public String birthplace;
+
+    public Person(int personId, int avatarIndex, String name, String country, String nickName, int startYear, int endYear, String birthplace) {
+        this.personId = personId;
+        this.avatarIndex = avatarIndex;
+        this.name = name;
+        this.country = country;
+        this.nickName = nickName;
+        this.startYear = startYear;
+        this.endYear = endYear;
+        this.birthplace = birthplace;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
